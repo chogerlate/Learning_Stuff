1e4e01b382a0fcc903478d20df7657842584bf0f
Chogerlate <siwaratlaopromger4444@gmail.com>
2023-09-22 06:53:01 +0700

    Create 242_Valid_Anagram.py

---
+++ Leetcode/Easy/242_Valid_Anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = sorted(s)
+        t = sorted(t)
+        if len(s) != len(t):
+            return False
+        for i in range(len(t)):
+            if t[i] != s[i] :
+                return False
+        return True
+                
+# Compare to best runtime solution: 
+# thought: use Counter to count the number of each character in the string
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s)==Counter(t)
